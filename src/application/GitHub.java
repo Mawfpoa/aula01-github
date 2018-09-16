49bc7d5ddb150445f5509ad4a3380f55a7b24dc3
Mawfpoa <mawf_poa@hotmail.com>
2018-09-16 17:12:25 -0300

    acrescentei bom dia

--- src/application/GitHub.java
+++ src/application/GitHub.java
@@ -3,7 +3,9 @@ package application;
 public class GitHub {
 
 	public static void main(String[] args) {
-		// TODO Stub de método gerado automaticamente
+		
+		
+		System.out.println("Bom dia!");
 
 	}
 
